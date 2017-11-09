0fa5b888bb90cdf7a37db3ad9f72c5afe214dbfd
JinheonBaek <Jinheon.Baek@outlook.kr>
2017-11-09 17:16:06 +0900

    Refix controls function in main.py to automatically run all detections

--- main.py
+++ main.py
@@ -3,12 +3,20 @@ import cv2
 import scene
 import obscene
 
-def obscene_detect():
-    img = cv2.imread('./cognitive/test.jpg')
+def obscene_detect(img = None):
     result = obscene.detection(isRaw = True, url = None, img = img)
+    print(result)
     
-def controls(shots = None):
-    pass
+def controls(path, filename, shots = None):
+    cap = cv2.VideoCapture()
+    cap.open(path + filename)
+
+    for i in range(shots[-1] + 1):
+        (ret_val, im_cap) = cap.read()
+        if i in shots:
+            #obscene_detect(im_cap)
+            pass
+    
     # Obscene detection function
     # Blood detection function
     # Movement detecion function
@@ -17,12 +25,12 @@ def main():
     path = "./video/"
     filename = "test_video.mp4"
 
-    obscene_detect()
-
     # Scene detection function
-    shots = scene.detection(path, filename)
+    # shots = scene.detection(path, filename)
     
-    controls(shots)
+    shots = [84, 140, 288, 318, 342, 367, 390, 417, 443, 659, 724, 847, 881, 929, 988, 1085, 1129, 1170, 1363, 1409, 1455, 1504, 1577, 1615, 1664, 1687, 1713, 1751, 1962]
+
+    controls(path, filename, shots)
 
 if __name__ == '__main__':
     main()
